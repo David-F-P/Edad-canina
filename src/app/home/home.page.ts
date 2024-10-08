ed8fb7c84d18b44122813f328ff6270a2e9450bf
David-F-P <davidbv645@gmail.com>
2024-10-08 00:18:52 -0600

    commit edad-canina

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonText, IonInput,IonButton, } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonText, IonInput,IonButton,FormsModule],
+})
+export class HomePage {
+
+  edad:number = 0;
+  resultado:string = '';
+
+  constructor() {}
+
+  calcular (){
+    let edadCanina:number = this.edad * 7 
+    this.resultado = 'La edad canina de su mascota es '  +  edadCanina +  ' años';
+  }
+}
